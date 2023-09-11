6456aca908df2df749fd01191a1190b7fabaced2
Dan Chen <dan.chen@boardware.com>
2023-09-11 12:22:40 +0800

    feat

--- middleware.go
+++ middleware.go
@@ -111,7 +111,7 @@ func AuthorizeByJWT(token string) Authentication {
 func CorsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(http.StatusNoContent)

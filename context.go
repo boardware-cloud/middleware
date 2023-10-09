0c53909f25dd4bcb83412d528b7ea13d7dbd55cd
Dan Chen <dan.chen@boardware.com>
2023-10-09 17:02:33 +0800

    feat

--- context.go
+++ context.go
@@ -1,15 +1,11 @@
 package middleware
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 )
 
 func Auth() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		auth := Authorize(c)
-		fmt.Println(auth)
 		c.Next()
 	}
 }

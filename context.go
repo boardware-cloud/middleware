4024faaa7f3e9e65ea3c0586fd89f99602af4ec0
Dan Chen <dan.chen@boardware.com>
2023-10-09 17:15:19 +0800

    feat

--- context.go
+++ context.go
@@ -1,17 +1,11 @@
 package middleware
 
 import (
-	"github.com/boardware-cloud/model/core"
 	"github.com/gin-gonic/gin"
 )
 
 func Auth() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		auth := Authorize(c)
-		if auth.Status == Authorized {
-			// account, _ := core.FindAccount(auth.AccountId)
-			c.Set("account", core.Account{Email: "good"})
-		}
 		c.Next()
 	}
 }

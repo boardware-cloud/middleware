dc48f813a8bc14d62ad4670d72a0b154a9c6ae61
Dan Chen <dan.chen@boardware.com>
2023-11-23 14:39:35 +0800

    feat

--- context.go
+++ context.go
@@ -4,32 +4,10 @@ import (
 	"github.com/boardware-cloud/model"
 	"github.com/boardware-cloud/model/core"
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
-	"gorm.io/gorm"
 )
 
-var db *gorm.DB
-var accountRepository core.AccountRepository
-
-func init() {
-	viper.SetConfigName("env")
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath("./config")
-	err := viper.ReadInConfig()
-	if err != nil {
-		panic(err)
-	}
-	user := viper.GetString("database.user")
-	password := viper.GetString("database.password")
-	host := viper.GetString("database.host")
-	port := viper.GetString("database.port")
-	database := viper.GetString("database.database")
-	db, err = model.NewConnection(user, password, host, port, database)
-	accountRepository = core.NewAccountRepository(db)
-	if err != nil {
-		panic(err)
-	}
-}
+var db = model.GetDB()
+var accountRepository = core.GetAccountRepository()
 
 func Auth() gin.HandlerFunc {
 	return func(c *gin.Context) {
